bb09ee51aa9b4aaa40dc41d9686da5e14b8a6693
cedricfonsat <79462084+CedricFonsat@users.noreply.github.com>
2022-06-13 10:10:51 +0200

    darkmode validé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,39 @@
 
+  function dark(){
+      console.log("test");
+      document.body.style.background = "url('assets/image/background.png')";
+      let hOne = document.querySelector("h1");
+      let p = document.querySelectorAll("p");
+    
+    
+      document.querySelector(".contact-dark").setAttribute('style','background-color : black !important');  
+    
+      hOne.style.color = 'black';
+    
+      for (let i = 0; i < p.length; i++) {
+        p[i].style.color = 'black';
+      }
+      let darkmode = document.querySelector("#darkmode").classList.add("d-none");
+      let lightmode = document.querySelector(".lightmode").classList.remove("d-none");
+       document.querySelector(".lightmode").classList.add("night");
+
+    }
+
+/*
+    function dark2(){
+      dark2 = false
+      if (dark2 == true) {
+        document.querySelector("link").href("assets/css/style-dark.css")
+      }else{
+        document.querySelector("link").href("assets/css/style.css")
+      }
+      dark2 = !dark2
+    }
+    */
+/*
       let more = document.getElementById('button-more');
       let text = document.querySelector('#text-more');
       let dots = document.querySelector('#dots');
-
 
 function myFunction() {
     var dots = document.getElementById("dots");
@@ -19,6 +50,9 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
-function dark(){
-  console.log("test");
-}
+  */
+
+
+
+
+
